47997d2ed732bec33f1f9dae4f7910ab279decf5
tinchk <tincho_984@hotmail.com>
2024-02-09 14:56:02 -0300

    cambios

--- index.php
+++ index.php
@@ -22,10 +22,33 @@
          <?php include_once "menu.php"; ?>
    </header>
        <main class="container">
-             
-
-    </main>
-    <footer class="container"></footer>
+         <div class="row">
+            <div class="col-12 text-center py-3">
+                <img src="images/cohete.svg" alt="cohete inicio" width="250px">
+            </div>
+             <div class="row">
+                <div class="col-12 text-center py-3">
+                    <p class="text-center"><span class="parrafostyle">Hola; Bienvenido seas @ mi sitio digital con mi info profesional</span></p>
+                </div>
+             </div> 
+         </div>
+    </div>    
+     </main>
+    <footer class="container mt-auto py-4">
+        <div class="row">
+            <div class="col-3">
+                <a href="https://github.com"><i class="fa-brands fa-github"></i></a>
+                <a href="https://linkedin.com"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-3">
+                Sponsor <a href="https://depcsuite.com/">DePC suite</a>
+            </div>
+            <div class="col-3">
+                <a href="mailto:marty20@yahoo.com.ar">marty20@yahoo.com.ar</a>
+            </div>
+            <a href="https://api.whatsapp.com/send?phone=+541121766798" target="_blank"><i
+                    class="fa-brands fa-whatsapp px-3 pt-3 pb-4"></i></a>
+    </footer>
 
 </body>
 
